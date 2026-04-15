c7ac428e68df7b6a316ccf9925de91190f111210
Pinky <shampreethi.s.2024.it@rajalakshmi.edu.in>
2026-04-15 22:05:17 +0530

    fixed db connection

--- app.py
+++ app.py
@@ -12,7 +12,8 @@ def get_db_connection():
         host=os.environ.get("DB_HOST"),
         user=os.environ.get("DB_USER"),
         password=os.environ.get("DB_PASSWORD"),
-        database=os.environ.get("DB_NAME")
+        database=os.environ.get("DB_NAME"),
+        port=int(os.environ.get("DB_PORT"))
     )
 
 # -------- LOGIN --------
